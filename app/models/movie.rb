11357aefec434e2f38e07765fb2cf5d2a684db17
Zhijing Wu <zw3155@columbia.edu>
2025-10-02 04:09:17 +0000

    Feature: Completed rating filtering logic and view

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,8 +6,10 @@ class Movie < ActiveRecord::Base
   
 
   def self.with_ratings(ratings_list)
-     if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
-      movies with those ratings
-     if ratings_list is nil, retrieve ALL movies
+    if ratings_list.blank?
+      return all
+    else
+      where(rating: ratings_list)
     end
+  end
 end
